7c1113af315cc08f54edba53bb38353febeef652
msg8065 <msg8065@nyu.edu>
2024-09-24 01:54:12 +0530

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -1,47 +1,49 @@
-# Import necessary modules
+# Msg8065 import socket module
 from socket import *
+# In order to terminate the program
 import sys
 
-def webServer(port=13331):
-    # Create a TCP/IP socket
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.bind(("localhost", port))
-    serverSocket.listen(5)
-    print(f"Server is running on port {port}...")
-
-    while True:
-        # Accept a new connection
-        connectionSocket, addr = serverSocket.accept()
-        try:
-            message = connectionSocket.recv(1024).decode()  # Receive the request
-            print(f"Received message: {message}")  # Debugging line
-            filename = message.split()[1]  # Extract the filename from the request
-            
-            # Open the requested file
-            with open(filename[1:], 'r') as f:  
-                outputdata = f.read()
-
-            # Send HTTP response headers
-            connectionSocket.sendall("HTTP/1.1 200 OK\r\n".encode())
-            connectionSocket.sendall("Content-Type: text/html\r\n".encode())
-            connectionSocket.sendall("\r\n".encode())  # End of headers
-
-            # Send the content of the requested file
-            connectionSocket.sendall(outputdata.encode())
-
-        except IOError:
-            # Handle file not found error
-            connectionSocket.sendall("HTTP/1.1 404 Not Found\r\n".encode())
-            connectionSocket.sendall("Content-Type: text/plain\r\n".encode())
-            connectionSocket.sendall("\r\n".encode())  # End of headers
-            connectionSocket.sendall("File not found.".encode())
-        except (ConnectionResetError, BrokenPipeError):
-            print("Connection was reset or broken.")
-        finally:
-            connectionSocket.close()  # Close the connection
-
-    serverSocket.close()
-    sys.exit()  # Terminate the program
+def webServer(local_host="127.0.0.1", port=13331):
+  # Create socket
+  server_socket = socket(AF_INET, SOCK_STREAM)
+  # Prepare server socket
+  server_socket.bind((local_host, port))
+  print(f"Using port: {port}")
+  # Accept connections; 10 connection in backlog
+  server_socket.listen(10)
+  # Server run
+  while True:
+    print('Ready to serve...')
+    # Accept an incoming connection request
+    connection_socket, addr = server_socket.accept()
+    try:
+      # Receive the request message
+      message = connection_socket.recv(1024).decode('utf-8')
+      # Get the filename from the received message
+      filename = message.split()[1]
+      f = open(filename[1:], 'rb')
+      file_data = f.read()
+      # Prepare the HTTP 200 OK response header and add the file contents
+      output_data = (
+        "HTTP/1.1 200 OK\r\n"
+        "CN WEBSERVER ASSIGNMENT\r\n"
+        "Content-Type: text/html; charset=UTF-8\r\n"
+        "Connection: close\r\n\r\n"
+      ).encode('utf-8') + file_data
+      # Send HTTP header and file content in one send
+      connection_socket.send(output_data)
+      connection_socket.close()
+    except Exception as e:
+      # Send response message for invalid request due to the file not being found (404)
+      error_message = b"<html><body><h1>404 Not Found</h1></body></html>"
+      output_data = (
+        "HTTP/1.1 404 Not Found\r\n"
+        "Server: Computer Networks: Programming\r\n"
+        "Content-Type: text/html; charset=UTF-8\r\n"
+        "Connection: close\r\n\r\n"
+      ).encode('utf-8') + error_message
+      connection_socket.send(output_data)
+      connection_socket.close()
 
 if __name__ == "__main__":
-    webServer(13331)
+  webServer()
