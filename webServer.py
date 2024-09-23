f2c66fbed48c1ea6a6075cb8573985ec044c1811
msg8065 <msg8065@nyu.edu>
2024-09-24 01:39:32 +0530

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -1,9 +1,5 @@
 GET /index.html HTTP/1.1
-Host: www.example.com
-User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3
-Accept-Language: en-US,en;q=0.9
-Accept-Encoding: gzip, deflate, br
-Connection: keep-alive
+
 # Import necessary modules
 from socket import *
 import sys
