9765d3557aed69c9dbbbf37cc7d7905e4bd74582
msg8065 <msg8065@nyu.edu>
2024-09-24 00:18:45 +0530

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -1,47 +1,67 @@
-# Import necessary modules
+# import socket module
 from socket import *
+# In order to terminate the program
 import sys
 
 def webServer(port=13331):
-    # Create a TCP/IP socket
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.bind(("localhost", port))
-    serverSocket.listen(5)
-    print(f"Server is running on port {port}...")
+  serverSocket = socket(AF_INET, SOCK_STREAM)
+  
+  #Prepare a server socket
+  serverSocket.bind(("", port))
+  
+  #Fill in start
+  serverSocket.listen(1)
+  #Fill in end
 
-    while True:
-        # Accept a new connection
-        connectionSocket, addr = serverSocket.accept()
-        try:
-            message = connectionSocket.recv(1024).decode()  # Receive the request
-            print(f"Received message: {message}")  # Debugging line
-            filename = message.split()[1]  # Extract the filename from the request
-            
-            # Open the requested file
-            with open(filename[1:], 'r') as f:  
-                outputdata = f.read()
+  while True:
+    #Establish the connection
+    
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept() #Fill in start -are you accepting connections?     #Fill in end
+    
+    try:
+      message = connectionSocket.recv(1024).decode() #Fill in start -a client is sending you a message   #Fill in end 
+      filename = message.split()[1]
+      
+      #opens the client requested file. 
+      #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
+      f = open(filename[1:], 'rb')#fill in start #fill in end)
+      #fill in end
+      
+      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"\
+      #Fill in start -This variable can store your headers you want to send for any valid or invalid request.
+      http_response = b""
+      #Content-Type above is an example on how to send a header as bytes
+      #Fill in end
 
-            # Send HTTP response headers
-            connectionSocket.sendall("HTTP/1.1 200 OK\r\n".encode())
-            connectionSocket.sendall("Content-Type: text/html\r\n".encode())
-            connectionSocket.sendall("\r\n".encode())  # End of headers
+      #Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok? 
+      #Fill in start
+      http_response = b"HTTP/1.1 200 OK\r\n"
+      connectionSocket.send(http_response)
+      connectionSocket.send(outputdata)
+      #Fill in end
 
-            # Send the content of the requested file
-            connectionSocket.sendall(outputdata.encode())
+      #Send the content of the requested file to the client
+      for i in f: #for line in file
+        connectionSocket.send(i)#Fill in start - send your html file contents #Fill in end 
+      connectionSocket.close() #closing the connection socket
+      
+    except Exception as e:
+      # Send response message for invalid request due to the file not being found (404)
+      #Fill in start
+      http_response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+      connectionSocket.send(http_response)
+      connectionSocket.send(outputdata)
+      #Fill in end
 
-        except IOError:
-            # Handle file not found error
-            connectionSocket.sendall("HTTP/1.1 404 Not Found\r\n".encode())
-            connectionSocket.sendall("Content-Type: text/plain\r\n".encode())
-            connectionSocket.sendall("\r\n".encode())  # End of headers
-            connectionSocket.sendall("File not found.".encode())
-        except (ConnectionResetError, BrokenPipeError):
-            print("Connection was reset or broken.")
-        finally:
-            connectionSocket.close()  # Close the connection
 
-    serverSocket.close()
-    sys.exit()  # Terminate the program
+      #Close client socket
+      #Fill in start
+      connectionSocket.close()
+      #Fill in end
+
+  serverSocket.close()
+  sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
-    webServer(13331)
+  webServer(13331)
